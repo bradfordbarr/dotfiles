dc53f829b676e8bf802d61eda3020efdd0a29035
Bradford Barr <bradford.barr@gmail.com>
2013-09-30 15:18:27 -0400

    Emacs config now completely handled by forked perlude.

--- emacs.d/init.el
+++
@@ -1,9 +0,0 @@
-(setq ispell-program-name "aspell")
-(add-to-list 'exec-path "/opt/boxen/homebrew/bin/")
-
-(setq tab-width 4
-      indent-tabs-mode t)
-
-(prelude-require-packages '(solarized-theme))
-
-(load-theme 'solarized-dark t)
